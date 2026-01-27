f7e7ae2f9cf89185111bf5307a0abc1d38062dbe
CynthiaBorotPNV <cynthia.borot@vanoise-parcnational.fr>
2026-01-27 15:28:29 +0100

    doc: Add dev documentation file

--- backend/gn_module_demo/blueprint.py
+++ backend/gn_module_demo/blueprint.py
@@ -2,6 +2,11 @@
 Définition des routes du module export
 """
 
+import logging
+from geonature.contrib.gn_module_validation.backend.gn_module_validation import schema
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import joinedload
+
 from flask import Blueprint
 from werkzeug.exceptions import NotFound
 
@@ -11,8 +16,11 @@ from geonature.utils.env import db
 from utils_flask_sqla.response import json_resp
 
 from . import MODULE_CODE
-from .models import Demo
+from .models import Demo, Individual
+from apptax.taxonomie.models import Taxref
+from .schemas import IndividualSchema
 
+logger = logging.getLogger(__name__)
 blueprint = Blueprint("demo", __name__, cli_group="demo")
 
 ## ########################################################################
@@ -45,3 +53,48 @@ def demo(id_demo):
     if demo is None:
         raise NotFound(f"Demo {id_demo} not found")
     return {"id_demo": demo.id_demo}
+
+# ########################################################################
+# Sans sérialiser marshmallow
+# 
+# @blueprint.route("/indiv", methods=["GET"])
+# @login_required
+# @json_resp
+# def indiv():
+#     query = db.select(
+#                 Individual.id_individual,
+#                 Individual.name,
+#                 Taxref.nom_complet
+#             ).select_from(Individual).join(Taxref, Individual.cd_nom == Taxref.cd_nom)
+#     # sql = query.compile(
+#     #     dialect=postgresql.dialect(),
+#     #         compile_kwargs={"literal_binds": True},
+#     # )
+#     # logger.info("SQL: %s", sql)
+
+#     indivs = db.session.execute(query).all()
+#     logger.info(f"------- indivs log: {indivs}")
+
+#     return [{"id_individual": indiv.id_individual, "name": indiv.name, "nom_complet": indiv.nom_complet} for indiv in indivs]
+
+# ########################################################################
+# Avec sérialiseur marshmallow
+# 
+@blueprint.route("/indiv", methods=["GET"])
+@login_required
+@json_resp
+def indiv():
+    # Un schéma doit être créé
+    schema = IndividualSchema()
+
+    query = db.select(Individual).options(joinedload(Individual.taxref))
+    # sql = query.compile(
+    #     dialect=postgresql.dialect(),
+    #         compile_kwargs={"literal_binds": True},
+    # )
+    # logger.info("SQL: %s", sql)
+   
+    indivs = db.session.execute(query).all()
+    logger.info(f"------- indivs log: {indivs}")
+
+    return schema.dump(indivs, many=True)
